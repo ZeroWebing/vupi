e15739b7bcd51104d1b0f44d594b8dd0a55e32e1
lldsShun <lldsshun@163.com>
2024-10-14 20:45:35 +0800

    ✨ feat: add PWA

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,11 +1,11 @@
 <template>
-    <span>home </span>
+    <div class="flex w-full h-100vh justify-center items-center">
+        <span class="text-lg">Vue3 + PrimeVue + UnoCSS</span>
+    </div>
 </template>
 
 <script setup lang='ts'>
 
 </script>
 
-<style>
-
-</style>
+<style></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+/** pwa */
+import { VitePWA } from 'vite-plugin-pwa'
+
 /** primeVue */
 import Components from 'unplugin-vue-components/vite'
 import { PrimeVueResolver } from '@primevue/auto-import-resolver'
-
 /** UnoCSS */
 import UnoCSS from 'unocss/vite'
 
@@ -11,6 +13,49 @@ import UnoCSS from 'unocss/vite'
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true
+      },
+      manifest: {
+        name: 'My App',
+        short_name: 'App',
+        theme_color: '#42b983',
+        icons: [
+          {
+            src: 'icon.svg',
+            sizes: '200x200',
+            type: 'image/svg+xml',
+            purpose: "any"
+          },
+          {
+            src: 'icon.png',
+            sizes: '200x200',
+            type: 'image/png',
+            purpose: "any"
+          },
+        ],
+        screenshots: [
+          {
+            src: 'screenshot-desktop.webp', // 用于桌面设备
+            sizes: '1920x1080',
+            type: 'image/webp',
+            form_factor: 'wide',
+          },
+          {
+            src: 'screenshot-mobile.webp',// 用于移动设备
+            sizes: '1080x1920',
+            type: 'image/webp',
+            form_factor: 'narrow',
+          },
+        ],
+      }
+    }),
     UnoCSS(),
     Components({
       resolvers: [
