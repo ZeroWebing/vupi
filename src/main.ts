2d8fcc179ba9d9ff5e51af368c36e551c195dedf
lldsShun <lldsshun@163.com>
2024-10-23 10:03:17 +0800

    fix ts error

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,25 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 // Router and store
-import router from "./router"
-import store from './store'
+import router from "@/router"
+import store from '@/store'
 
 // Styles
-import './style/index.scss'
+import '@/style/index.scss'
 
 // PrimeVue UI
 import PrimeVue from 'primevue/config'
-import MyPreset from './style/primevue/myPreset'
+import MyPreset from '@/style/primevue/myPreset'
 import 'primeicons/primeicons.css'
 
 // UnoCSS
 import "uno.css"
 
+// vueuse/motion
 import { MotionPlugin } from '@vueuse/motion'
-
 
 // Create and mount app
 const app = createApp(App)
-
 app
   .use(router)
   .use(store)
