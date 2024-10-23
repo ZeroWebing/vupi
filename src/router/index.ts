359f14df1bab23e39051f4f7a9447653ce7628e1
lldsShun <lldsshun@163.com>
2024-10-23 22:34:59 +0800

    ✨ feat: home page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,9 @@
 import { createWebHistory, createRouter } from 'vue-router'
 
 import HomePage from '@/pages/home/index.vue'
-import Ex1Page from '@/pages/ex-page1/index.vue'
 
 const routes = [
     { path: '/', component: HomePage },
-    { path: '/ex1', component: Ex1Page },
 ]
 
 const router = createRouter({
